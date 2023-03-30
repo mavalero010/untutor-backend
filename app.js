6a7f1550582e6e904a89b3e62f90cf460d7530cf
Jhon Álvarez <jwalvarez@uninorte.edu.co>
2023-03-29 21:59:59 -0500

    Format files

--- app.js
+++ app.js
@@ -1,29 +1,24 @@
-const express = require('express');
-const { connectDB } = require('./src/config/db.config')
+const express = require("express");
+const { connectDB } = require("./src/config/db.config");
 
-const dotenv = require("dotenv")
-const routerUser = require ("./src/routes/user_route")
-const cors = require('cors')
-const {json} = require('express')
+const dotenv = require("dotenv");
+const routerUser = require("./src/routes/user_route");
 
-connectDB()
+connectDB();
 
-const app = express()
-dotenv.config() 
+const app = express();
+dotenv.config();
 
-app.use(cors())
-app.use(json())
-app.use(express.static('./public'))
-app.use(express.json())
-app.use(express.urlencoded({ extended: false }))
+app.use(express.static("./public"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
-app.get('/api', (req, res) => {
-  res.send('API USER')
-})
+app.get("/api", (req, res) => {
+  res.send("API USER");
+});
 
-
-app.use(routerUser)
+app.use(routerUser);
 
 app.listen(process.env.PORT, () => {
-  console.log(`Example app listening on port ${process.env.PORT}`)
-})
+  console.log(`Example app listening on port ${process.env.PORT}`);
+});
--- src/models/story_model.js
+++ src/models/story_model.js
@@ -1,16 +1,20 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
-const storySchema = new mongoose.Schema({
-    
-    name:{type:String,require:true},
-    IDUser:{type:mongoose.Schema.ObjectId,ref:'User', require:true},
-    multimedia:{type:String, require:true},//TODO:Especificar tipo de dato
-    ID_comment_list:[{
-        type:mongoose.Schema.ObjectId,
-        ref:'Comment'}]
-},{
-      versionKey:false
-    })
+const storySchema = new mongoose.Schema(
+  {
+    name: { type: String, require: true },
+    IDUser: { type: mongoose.Schema.ObjectId, ref: "User", require: true },
+    multimedia: { type: String, require: true }, //TODO:Especificar tipo de dato
+    ID_comment_list: [
+      {
+        type: mongoose.Schema.ObjectId,
+        ref: "Comment",
+      },
+    ],
+  },
+  {
+    versionKey: false,
+  }
+);
 
-
-module.exports = mongoose.model('Story', storySchema)
+module.exports = mongoose.model("Story", storySchema);
