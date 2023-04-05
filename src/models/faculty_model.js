afc728a4d97b5eb2ea59078f5b347e689971a1e7
mavalero010 <maicolvpolo@gmail.com>
2023-04-04 22:18:11 -0500

    Create y Update Subject en rol de admin

--- src/models/faculty_model.js
+++ src/models/faculty_model.js
@@ -3,7 +3,12 @@ const mongoose = require("mongoose")
 const facultySchema = new mongoose.Schema(
     {
       name: { type: String, require: true, unique:true },
-      description: { type: String, require: true }
+      description: { type: String, require: true },
+      iduniversity:{
+        type: mongoose.Schema.ObjectId,
+        ref: "University",
+        require: true,
+      }
       
     },
     {
